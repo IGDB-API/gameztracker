62452e0f26855206bb5eb60b031084961f9c245f
StarGale <zhenchix@hotmail.com>
2018-09-27 00:46:40 -0700

    fix silly img extension glitch

--- scripts/api.js
+++ scripts/api.js
@@ -15,14 +15,14 @@ $(function populateNews() {
         newsResults = res;
         for (let i in res.articles) {
             /////placeholder for missing img/////
-            let coverUrl = "../src/NoImageAvailable.jpg";
+            // let coverUrl = "../src/NoImageAvailable.jpg";
             /////placeholder for missing img/////
 
             let title = newsResults.articles[i].title;
             let author = newsResults.articles[i].author;
             let description = newsResults.articles[i].description;
             let url = newsResults.articles[i].url;
-            let urlToImage = newsResults.articles[i].urlToImage || coverUrl;
+            let urlToImage = newsResults.articles[i].urlToImage;
             let publishedAt = newsResults.articles[i].publishedAt;
             let source = newsResults.articles[i].source.name;
 
@@ -76,20 +76,20 @@ var igdbSettings = {
 
 function populatePopular() {
     $.ajax(igdbSettings).then((res, status) => {
-        console.log(res);
-        console.log(status);
+        // console.log(res);
+        // console.log(status);
         popularResults = res;
         for (let i in popularResults) {
             let id = popularResults[i].id;
             let name = popularResults[i].name;
             let coverUrl = popularResults[i].cover.cloudinary_id;
-
+            // console.log(coverUrl);
             $('#popular-games').append(
                 `
                 <li>
                 <div class="game-preview">
                     <p>${name}</p>
-                    <div><img src="https://images.igdb.com/igdb/image/upload/t_cover_big/${coverUrl}" id="${id}"/></div>
+                    <div><img src="https://images.igdb.com/igdb/image/upload/t_cover_big/${coverUrl}.png" id="${id}"/></div>
                 </div>
                 </li>
                 `
@@ -128,7 +128,7 @@ $('#popular-games').on('click', 'img', function () {
             $("#popular-games-detail").append(
                 `
                 <div>
-                <img src="https://images.igdb.com/igdb/image/upload/t_cover_big/${coverUrl}"/>
+                <img src="https://images.igdb.com/igdb/image/upload/t_cover_big/${coverUrl}.png"/>
                 
                 <h2>${name}</h2><span class="fas fa-window-close"></span>
                 <div>
@@ -189,10 +189,10 @@ $('#quick-search-form').on('submit', function (e) {
                 if (typeof (quickSearchResults[i].cover) == "undefined") {
                     var coverUrl = "../src/NoImageAvailable.jpg";
 
-                    console.log(coverUrl);
+                    // console.log(coverUrl);
                 }
                 else {
-                    var coverUrl = "https://images.igdb.com/igdb/image/upload/t_logo_med/" + quickSearchResults[i].cover.cloudinary_id;
+                    var coverUrl = `https://images.igdb.com/igdb/image/upload/t_logo_med/${quickSearchResults[i].cover.cloudinary_id}.png`;
                     console.log(coverUrl);
                 }
 
@@ -233,7 +233,7 @@ $('#quick-search-popup').on('click', 'img', function () {
                 console.log(coverUrl);
             }
             else {
-                var coverUrl = "https://images.igdb.com/igdb/image/upload/t_cover_big/" + quickSearchResults[j].cover.cloudinary_id;
+                var coverUrl = `https://images.igdb.com/igdb/image/upload/t_cover_big/${quickSearchResults[j].cover.cloudinary_id}.png`;
                 console.log(coverUrl);
             }
             let summary = quickSearchResults[j].summary;
